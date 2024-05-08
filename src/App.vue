93719aff52d36c51df2026c76f5670d843d9c5db
Sandra Katharina <joceepaul@me.com>
2024-05-08 23:44:04 +0200

    add method to button when clicked to reload page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1>Title: {{ artObject.title }}, dated :{{ artObject.objectDate }}</h1>
-    <div v-if="artObject.primaryImageSmall.length == 0">Sorry no image</div>
-    <img v-else :src="artObject.primaryImageSmall" />
+    <div v-if="artObject.primaryImageSmall == 0">Sorry no image</div>
+    <img v-else-if="artObject.primaryImageSmall" :src="artObject.primaryImageSmall" />
   </div>
   <button @click="showRandomArtObject">SHOW ME SOMETHING RANDOM!</button>
 </template>
@@ -17,6 +17,12 @@ export default {
     return {
       artObject: {},
       resultList: {}
+    }
+  },
+  methods: {
+    showRandomArtObject(value) {
+      console.log(value)
+      window.location.reload()
     }
   },
   async created() {
